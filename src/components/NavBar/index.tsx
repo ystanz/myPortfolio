67cf611fccb7180565046869e96ab8b7394ba5f5
Yuri Stanzani <ystanzani@gmail.com>
2025-03-14 22:20:58 -0300

    feat: create NavBar component

---
+++ src/components/NavBar/index.tsx
@@ -0,0 +1,18 @@
+import { Nav, NavItem } from './styles'
+
+const NavBar = () => {
+  return (
+    <>
+      <Nav>
+        <NavItem>Início</NavItem>
+        <NavItem>Sobre mim</NavItem>
+        <NavItem>Experiências</NavItem>
+        <NavItem>Projetos</NavItem>
+        <NavItem>Certificações</NavItem>
+        <NavItem>Habilidades</NavItem>
+      </Nav>
+    </>
+  )
+}
+
+export default NavBar
---
+++ src/components/NavBar/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const Nav = styled.ul`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  color: #fff;
+`
+export const NavItem = styled.li`
+  margin-right: 32px;
+`
